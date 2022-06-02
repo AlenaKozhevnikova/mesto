84280ad86444b514a2b3617b500dfc5ef12e5380
Alena Kozhevnikova <alenakinder@gmail.com>
2022-06-02 14:54:46 +0900

    закончила фиксить

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,35 @@
 const editButton = document.querySelector('.profile__edit-button')
 const popup = document.querySelector('.popup')
 const closeButton = document.querySelector('.popup__close')
-let formElement = document.querySelector('.popup__form')
+const formElement = document.querySelector('.popup__form')
 const profileName = document.querySelector('.profile__name')
 const profileDescription = document.querySelector('.profile__description')
-const nameFieldElement = document.querySelector('.popup__input_name')
-const descriptionFieldElement = document.querySelector('.popup__input_description')
 
-function openPopup(popupElement) {
+function openPopup() {
   popup.classList.add('popup_open')
 }
 
-function closePopup(popupElement) {
+function closePopup() {
   popup.classList.remove('popup_open')
 }
 
 editButton.addEventListener('click', function() {
-  openPopup(popup)
+  openPopup()
   nameFieldElement.value = profileName.textContent;
   descriptionFieldElement.value = profileDescription.textContent;
 })
 
 closeButton.addEventListener('click', function() {
-  closePopup(popup)
+  closePopup()
 })
+
+const nameFieldElement = document.querySelector('.popup__input_name')
+const descriptionFieldElement = document.querySelector('.popup__input_description')
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
     profileName.textContent = nameFieldElement.value;
     profileDescription.textContent = descriptionFieldElement.value;
-    closePopup(popup)
+    closePopup()
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
